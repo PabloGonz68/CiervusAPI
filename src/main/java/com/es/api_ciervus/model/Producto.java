0bfa39622680106a666e51ef48e0f5f14768734b
PabloGonz <pablogonzalezsilva6@gmail.com>
2024-12-11 17:43:39 +0100

    Añadido uso de Lombok

--- src/main/java/com/es/api_ciervus/model/Producto.java
+++ src/main/java/com/es/api_ciervus/model/Producto.java
@@ -1,10 +1,18 @@
 package com.es.api_ciervus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "productos")
 public class Producto {
     @Id
@@ -21,71 +29,11 @@ public class Producto {
     @JoinColumn(name = "propietario_id", nullable = false)
     private Usuario propietario_id;
 
-    public Producto() {
-    }
-
-    public Producto(Long id, String nombre, String descripcion, double precio, Date fecha_publicacion, Usuario propietario_id) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.fecha_publicacion = fecha_publicacion;
-        this.propietario_id = propietario_id;
-    }
-
     public Producto(String nombre, String descripcion, double precio, Date fecha_publicacion, Usuario propietario_id) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.fecha_publicacion = fecha_publicacion;
-        this.propietario_id = propietario_id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public Date getFecha_publicacion() {
-        return fecha_publicacion;
-    }
-
-    public void setFecha_publicacion(Date fecha_publicacion) {
-        this.fecha_publicacion = fecha_publicacion;
-    }
-
-    public Usuario getPropietario_id() {
-        return propietario_id;
-    }
-
-    public void setPropietario_id(Usuario propietario_id) {
         this.propietario_id = propietario_id;
     }
 }
--- src/main/java/com/es/api_ciervus/model/Reserva.java
+++ src/main/java/com/es/api_ciervus/model/Reserva.java
@@ -1,10 +1,18 @@
 package com.es.api_ciervus.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Table(name = "reservas")
 public class Reserva {
     @Id
@@ -21,18 +29,6 @@ public class Reserva {
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario_id;
 
-
-    public Reserva() {
-    }
-
-    public Reserva(Long id, Date fecha_inicio, Date fecha_fin, Producto producto_id, Usuario usuario_id) {
-        this.id = id;
-        this.fecha_inicio = fecha_inicio;
-        this.fecha_fin = fecha_fin;
-        this.producto_id = producto_id;
-        this.usuario_id = usuario_id;
-    }
-
     public Reserva(Date fecha_inicio, Date fecha_fin, Producto producto_id, Usuario usuario_id) {
         this.fecha_inicio = fecha_inicio;
         this.fecha_fin = fecha_fin;
@@ -40,43 +36,4 @@ public class Reserva {
         this.usuario_id = usuario_id;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Date getFecha_inicio() {
-        return fecha_inicio;
-    }
-
-    public void setFecha_inicio(Date fecha_inicio) {
-        this.fecha_inicio = fecha_inicio;
-    }
-
-    public Date getFecha_fin() {
-        return fecha_fin;
-    }
-
-    public void setFecha_fin(Date fecha_fin) {
-        this.fecha_fin = fecha_fin;
-    }
-
-    public Producto getProducto_id() {
-        return producto_id;
-    }
-
-    public void setProducto_id(Producto producto_id) {
-        this.producto_id = producto_id;
-    }
-
-    public Usuario getUsuario_id() {
-        return usuario_id;
-    }
-
-    public void setUsuario_id(Usuario usuario_id) {
-        this.usuario_id = usuario_id;
-    }
 }
